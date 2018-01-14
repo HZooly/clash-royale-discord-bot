0f37998a5eebab85eb441ae65084ea08c2faa8d0
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2018-01-14 18:26:52 +0100

    :recycle: Refactor functions into external file

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const axios = require('axios');
 const conf = require('./conf.json');
 
+const Utils = require('./utils.js');
+
 const bot = new Discord.Client();
 const clanTag = conf.clanTag;
 
@@ -23,26 +25,12 @@ bot.on('message', message => {
         args = args.splice(1);
         switch (instruction) {
             case 'hello':
-                message.channel.send(`Hello <@${message.author.id}>!`);
+                Utils.hello(message);
                 break;
             case 'clan':
                 request.get(`http://api.cr-api.com/clan/${clanTag}`)
                     .then(res => {
-                        const data = res.data;
-                        const embed = new Discord.RichEmbed();
-                        embed.setTitle("Clan Informations")
-                            .setDescription(data.description)
-                            .setColor("#22A7F0")
-                            .addField("Name", data.name)
-                            .addField("Players", `${data.memberCount}/50`)
-                            .addField("Score", data.score)
-                            .addField("Donations", data.donations)
-                            .addField("Required Trophies", data.requiredScore)
-                            .setThumbnail(data.badge.image);
-
-                        message.channel.send({
-                            embed
-                        });
+                        Utils.clan(res.data, message, Discord);
                     })
                     .catch(err => {
                         console.log(err)
@@ -51,43 +39,13 @@ bot.on('message', message => {
             case 'top5':
                 request.get(`http://api.cr-api.com/clan/${clanTag}`)
                     .then(res => {
-                        const data = res.data.members;
-                        const embed = new Discord.RichEmbed()
-                            .setTitle("Top Players")
-                            .setDescription("5 High ranked players");
-                        for (let i = 0; i < data.length && i < 5; i++) {
-                            embed.addField(
-                                `${i+1}. ${data[i].name}`,
-                                `\:trophy: ${data[i].trophies}`
-                            );
-                        }
-                        message.channel.send({
-                            embed
-                        })
+                        Utils.top5(res.data.members, message, Discord);
                     });
                 break;
             case 'donations':
                 request.get(`http://api.cr-api.com/clan/${clanTag}`)
                     .then(res => {
-                        const data = res.data.members;
-                        const embed = new Discord.RichEmbed()
-                            .setTitle("Top Donations Player")
-                            .setDescription("5 Best Donations Players");
-
-                        data.sort((a, b) => {
-                            return b.donations - a.donations
-                        });
-
-                        for (let i = 0; i < data.length && i < 5; i++) {
-                            embed.addField(
-                                `${i+1}. ${data[i].name}`,
-                                `\:gift: ${data[i].donations}`
-                            );
-                        }
-
-                        message.channel.send({
-                            embed
-                        })
+                        Utils.donations(res.data.members, message, Discord);
                     });
                 break;
             default:
